e636e094323a331d9e7e8d6a42c57b763cb0ae61
Kstadhammer <109215347+Kstadhammer@users.noreply.github.com>
2025-09-12 15:04:21 +0200

    Update DTOs and fix file structure

---
+++ Domain/Entities/Event.cs
@@ -0,0 +1,12 @@
+public class Event
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public DateTime StartTime { get; set; }
+    public DateTime EndTime { get; set; }
+    public string InstructorName { get; set; }
+    public int MaxParticipants { get; set; }
+    public int AvailableSpots { get; set; }
+    public string Location { get; set; }
+}

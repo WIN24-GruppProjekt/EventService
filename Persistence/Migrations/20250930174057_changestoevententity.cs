8368b56898503b92e1c36772a9dc0d969861494d
Josip1123 <91788732+Josip1123@users.noreply.github.com>
2025-09-30 19:42:41 +0200

    migrations with updated event entity

---
+++ Persistence/Migrations/20250930174057_changestoevententity.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class changestoevententity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LocationRoom",
+                table: "Events");
+
+            migrationBuilder.RenameColumn(
+                name: "Location",
+                table: "Events",
+                newName: "TrainerId");
+
+            migrationBuilder.AddColumn<string>(
+                name: "LocationId",
+                table: "Events",
+                type: "nvarchar(200)",
+                maxLength: 200,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<int>(
+                name: "RoomId",
+                table: "Events",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LocationId",
+                table: "Events");
+
+            migrationBuilder.DropColumn(
+                name: "RoomId",
+                table: "Events");
+
+            migrationBuilder.RenameColumn(
+                name: "TrainerId",
+                table: "Events",
+                newName: "Location");
+
+            migrationBuilder.AddColumn<string>(
+                name: "LocationRoom",
+                table: "Events",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
